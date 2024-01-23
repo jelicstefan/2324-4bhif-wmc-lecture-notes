bf52f20b9dd8b9c2eae4a567761b7219d979b625
htl-leonding <t.stuetz@htl-leonding.ac.at>
2024-01-23 20:03:02 +0100

    lecture notes 2024-01-23 - lauffähig - use hilt version 2.48

--- labs/MyFirstApp/app/build.gradle.kts
+++ labs/MyFirstApp/app/build.gradle.kts
@@ -71,8 +71,8 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
 
     // RxJava
     implementation("io.reactivex.rxjava3:rxjava:3.1.8")
--- labs/MyFirstApp/app/src/main/java/at/ac/htl/myfirstapp/ui/layout/MainView.kt
+++ labs/MyFirstApp/app/src/main/java/at/ac/htl/myfirstapp/ui/layout/MainView.kt
@@ -7,8 +7,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rxjava3.subscribeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import at.ac.htl.myfirstapp.model.Model
+import at.ac.htl.myfirstapp.model.Store
 import at.ac.htl.myfirstapp.ui.theme.MyFirstAppTheme
 import dagger.hilt.android.qualifiers.ActivityContext
 import javax.inject.Inject
@@ -17,35 +20,37 @@ import javax.inject.Singleton
 @Singleton
 class MainView @Inject constructor() {
 
+    @Inject
+    lateinit var store: Store
 
     fun compose(activity: ComponentActivity) {
         activity.setContent {
             MyFirstAppTheme {
+                val state = store.subject.subscribeAsState(initial = Model())
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(state.value.greeting)
                 }
             }
         }
     }
-
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        MyFirstAppTheme {
-            Greeting("Android")
-        }
-    }
 }
 
+@Composable
+fun Greeting(greeting: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $greeting!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyFirstAppTheme {
+        Greeting("Android")
+    }
+}
