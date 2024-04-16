6e5f2bd68c12e256657b4ed5fc8c015588025343
htl-leonding <t.stuetz@htl-leonding.ac.at>
2024-04-16 12:35:27 +0200

    Button zum Laden der Todos mit Anzeige (routerLink)

--- labs/ng-demo/ng-demo/src/app/app.component.ts
+++ labs/ng-demo/ng-demo/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, inject, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {StoreService} from "./services/store.service";
 import {TodoService} from "./services/todo.service";
 
@@ -7,7 +7,7 @@ import {TodoService} from "./services/todo.service";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,6 +16,10 @@ export class AppComponent implements OnInit {
   todoService = inject(TodoService)
 
   ngOnInit() {
+    //this.todoService.findAll()
+  }
+
+  loadTodos() {
     this.todoService.findAll()
   }
 }
